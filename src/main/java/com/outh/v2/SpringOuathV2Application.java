aa387d2e0cb63460eb644914cc55671fdfbe0533
Jose Plasencia <jose.plasencia@externos.correo.gob.es>
2024-07-04 20:44:57 +0200

    ini

---
+++ src/main/java/com/outh/v2/SpringOuathV2Application.java
@@ -0,0 +1,13 @@
+package com.outh.v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOuathV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOuathV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/outh/v2/SpringOuathV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outh.v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOuathV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
